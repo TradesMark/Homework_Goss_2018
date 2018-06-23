f66ef3953eb68a57dd63cc1ef8ec924af412c975
Anton Galkin <artemis9898@mail.ru>
2018-06-23 15:27:52 +0300

    Add files via upload

---
+++ KP_2018/13.03.18/task_2/index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const moment = require('moment');
+
+http.createServer((req, res)=>{
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.end(JSON.stringify({date: moment().format('DD.MM.YYYY HH:mm:ss') }));
+}).listen(4321);
